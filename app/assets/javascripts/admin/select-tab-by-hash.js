f9959578c7257f51cc0ca4d768ffd20554416c33
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-09-23 07:13:50 +0400

    tabs persistant fix

--- app/assets/javascripts/admin/select-tab-by-hash.js
+++ app/assets/javascripts/admin/select-tab-by-hash.js
@@ -4,9 +4,19 @@ var TabByHash = {
 	}
 };
 
+var Tabbable = function(el) {
+	el.on('click', function() {
+		window.location.hash = el.attr('href');
+	});
+};
+
 $(function() {
 	TabByHash.select(window.location.hash);
-	
+
+	$('[data-toggle=tab]').each(function() {
+		new Tabbable($(this));
+	});
+
 	$(window).on('hashchange', function() {
 		TabByHash.select(window.location.hash);
 	})
