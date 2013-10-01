c099ee17fc60ea3a939c0c4016e840da1827f808
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-10-01 16:11:37 +0400

    defaul seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ end
 page_types = YAML::load_file File.join(Rails.root, 'db', 'seeds', 'page_types.yml')
 
 page_types.each do |t|
-  page_type = PageType.find_or_create_by_name t[:name]
+  page_type = PageType.find_or_create_by_method t
 
   t.each do |k, v|
     page_type.update_attribute k, v
@@ -24,9 +24,46 @@ end
 colors = YAML::load_file File.join(Rails.root, 'db', 'seeds', 'blog_colors.yml')
 
 colors.each do |t|
-  color = BlogColor.find_or_create_by_name t[:name]
+  color = BlogColor.find_or_create_by_name t
 
   t.each do |k, v|
     color.update_attribute k, v
+  end
+end
+
+
+pages = YAML::load_file File.join(Rails.root, 'db', 'seeds', 'pages.yml')
+
+pages.each do |t|
+  t['page_type'] = PageType.find_by_name t['page_type']
+
+  page = Page.find_or_create_by_name_and_url t
+
+  t.each do |k, v|
+    page.update_attribute k, v
+  end
+end
+
+menus = YAML::load_file File.join(Rails.root, 'db', 'seeds', 'menus.yml')
+
+menus.each do |m|
+  items = m["items"]
+  m.except! "items"
+  
+  menu = Menu.find_or_create_by_key m
+
+  m.each do |k, v|
+    menu.update_attribute k, v
+  end
+
+  items.each do |i|
+    i["page"] = Page.find_by_name i['page']
+    i["menu"] = menu
+
+    menu_item = MenuItem.find_or_create_by_name i
+
+    i.each do |k, v|
+      menu_item.update_attribute k, v
+    end    
   end
 end
