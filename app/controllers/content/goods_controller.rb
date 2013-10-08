23538d55b1f5440936e266e09b6d502829ece75b
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-10-08 16:31:11 +0400

    no nag at good

--- app/controllers/content/goods_controller.rb
+++ app/controllers/content/goods_controller.rb
@@ -1,6 +1,5 @@
 class Content::GoodsController < Content::BaseController
   before_action :get_locale
-  before_action :navigation, only: [:item]
 
   def item
     @good = Good.find_by_slug params[:slug]
@@ -13,18 +12,4 @@ class Content::GoodsController < Content::BaseController
     @good
   end
 
-  private
-    def navigation
-      links = []
-
-      GoodCategory.all.each do |i|
-        links << { active: (true if @category == i), href: catalog_item_path(i.slug), title: i.title }
-      end
-
-      @navigation = {
-        helper: t('navigation.catalog.helper'),
-        title: t('navigation.catalog.title'),
-        items: links
-      }
-    end  
 end
