7e26623447b8ef069e96414916786aa661c7ced5
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-10-14 16:53:20 +0400

    поиск

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,17 @@
 class SearchController < Content::BaseController
+  include ActionView::Helpers::TextHelper
+
   def search
-    @query = params[:search]
+    @query = params[:search].downcase
     @results = []
+
+    Good.with_translations.where('lower(title) like ?', "%#{@query}%").each do |good|
+      @results << { picture: good.thumb.url(:preview), 
+                    title: good.title, 
+                    location: 'Каталог', 
+                    url: goods_item_path(good.slug), 
+                    content: truncate(strip_tags(good.announce), length: 300)
+                  }
+    end
   end
 end
