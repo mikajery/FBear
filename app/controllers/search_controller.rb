4dc88318e6f67ca996801a7706be088c68901bd5
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-10-21 11:36:51 +0400

    пустой поиск ищет все

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,13 +5,15 @@ class SearchController < Content::BaseController
     @query = params[:q].downcase
     @results = []
 
-    Good.with_translations.where('lower(title) like ?', "%#{@query}%").each do |good|
-      @results << { picture: good.thumb.url(:preview), 
-                    title: good.title, 
-                    location: 'Каталог', 
-                    url: goods_item_path(good.slug), 
-                    content: good.announce
-                  }
+    unless @query.empty?
+      Good.with_translations.where('lower(title) like ?', "%#{@query}%").each do |good|
+        @results << { picture: good.thumb.url(:preview),
+                      title: good.title,
+                      location: 'Каталог',
+                      url: goods_item_path(good.slug),
+                      content: good.announce
+                    }
+      end
     end
   end
 end
