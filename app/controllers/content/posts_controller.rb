b18a173a1b856816f2e81d5b4df9c0bc240b17ef
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-10-25 19:59:59 +0400

    blogs fixes

--- app/controllers/content/posts_controller.rb
+++ app/controllers/content/posts_controller.rb
@@ -1,6 +1,10 @@
 class Content::PostsController < Content::BaseController
   before_action :get_locale
 
+  def get_item
+    @post
+  end
+
   def item
     @post = Post.find_by_slug params[:slug]
     @comments = @post.comments.where( locale: I18n.locale )
