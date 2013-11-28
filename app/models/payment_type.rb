28fa53884e5ddb9b65a30625e99ddbeea4f6b695
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-11-28 08:21:44 +0400

    Форма заказа - типы оплаты - типы доставки

--- app/models/payment_type.rb
+++ app/models/payment_type.rb
@@ -4,6 +4,8 @@ class PaymentType < ActiveRecord::Base
 
   default_scope { order(:weight) }
 
+  scope :active, -> { where(is_active: true) }
+
   validates :type, presence: true, uniqueness: true
   validates :name, presence: true
 
