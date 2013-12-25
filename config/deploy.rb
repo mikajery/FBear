c339918dbd7adb7607841999a3d789e6b454288f
Ilya Doroshin <ilya.doroshin@gmail.com>
2013-12-26 03:37:07 +0400

    assets fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ end
 
 set (:unicorn_conf) {"./config/unicorn/#{stage_name}.rb"}
 set (:unicorn_pid) {"#{shared_path}/pids/unicorn.pid"}
-set (:unicorn_start_cmd) {"(cd #{deploy_to}current; rvm use 2.0.0 do bundle exec unicorn_rails -Dc #{unicorn_conf})"}
+set (:unicorn_start_cmd) {"(cd #{deploy_to}current; rvm use 2.0.0 do bundle exec unicorn_rails -Dc #{unicorn_conf} -E #{unicorn_env}) "}
 
 set :bundle_cmd, 'rvm use 2.0.0 do bundle'
 
