3486912b3f2e0f55e036b772d23e6e28f1fd3432
Ilya Doroshin <ilya.doroshin@gmail.com>
2014-01-13 14:08:24 +0400

    assets fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ CMS::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(admin.js frontend.js map.js backbone/* admin.css.scss content.css.scss map.css.scss landing.css.scss)
+  config.assets.precompile += %w(admin.js frontend.js map.js backbone/* admin.css admin.css.scss content.css content.css.scss map.css map.css.scss landing.css landing.css.scss)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
