a5516c6756b078e2058467bc9c0f140d2dc60ff5
Ilya Doroshin <ilya.doroshin@gmail.com>
2014-01-16 03:55:33 +0400

    В селекте в каждой строке сначала фотография (размер как у изображения доп товара) потом название

--- app/assets/javascripts/frontend/select2.js
+++ app/assets/javascripts/frontend/select2.js
@@ -1,7 +1,36 @@
+
 var FancySelect = function(el) {
-    el.select2({
+    var options = {
         minimumResultsForSearch: -1
-    });
+    };
+
+    var thumbed = function(state) {
+        var originalOption = state.element;
+
+        var table = $('<table>').addClass('variant-option'),
+            row = $('<tr>'),
+            html = state.text
+        ;
+
+        if ($(originalOption).data('thumb'))
+        {
+            var thumb = $('<td>').append($('<img>').attr('src', $(originalOption).data('thumb')));
+            var txt = $('<td>').html(state.text);
+            table.append(row.append(thumb).append(txt));
+            html = $('<div>').append(table).html();
+        }
+
+        return html;
+    }
+
+    if (el.data('format') == 'thumbed')
+    {
+        options['formatResult'] = thumbed;
+        options['formatSelection'] = thumbed;
+        options['escapeMarkup'] = function(m) { return m; }
+    }
+
+    el.select2(options);
 };
 
 $(document).on('ready page:load', function() {
