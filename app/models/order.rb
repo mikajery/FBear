473a3b15e2dd486aeacd45d527b9587683e31828
Ilya Doroshin <ilya.doroshin@gmail.com>
2014-01-23 13:00:39 +0400

    bugfix

--- app/models/order.rb
+++ app/models/order.rb
@@ -58,10 +58,12 @@ class Order < ActiveRecord::Base
   # стоимость доставки
   def delivery_price
     price = 0
-    if self.delivery_type.is_cost_calc_needed?
-      price += self.delivery_request.price if self.delivery_request.present?
-    else
-      price += self.delivery_type.price if self.delivery_type.present?
+    if self.delivery_type.present?
+      if self.delivery_type.is_cost_calc_needed?
+        price += self.delivery_request.price if self.delivery_request.present?
+      else
+        price += self.delivery_type.price if self.delivery_type.present?
+      end
     end
     price
   end
