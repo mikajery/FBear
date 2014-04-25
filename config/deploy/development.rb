63ee81105d26cd16f3683083e2efdc2a2a226a80
Iliya Stepanov <Kristijonas@Springenderpunkt.local>
2014-04-25 18:29:08 +0400

    change staging environment

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,8 +2,8 @@ role :web, 'dev.lllooch.ru' # Your HTTP server, Apache/etc
 role :app, 'dev.lllooch.ru' # This may be the same as your `Web` server
 role :db,  'dev.lllooch.ru', :primary => true        # This is where Rails migrations will run
 
-set :branch, 			'master'
-set :repository_branch, 'master'
+set :branch, 			'staging'
+set :repository_branch, 'staging'
 set :stage_name, 		'development'
 set :unicorn_env,   'production'
 set :rails_env, 		'production'
